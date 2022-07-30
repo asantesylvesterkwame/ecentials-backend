757ded1115d4acd29dbb38ed5a9b0053f25252b0
lateofrederick <lateofrederick@gmail.com>
2022-07-30 15:47:01 +0000

    add route to allow pharmacist upload prescription

--- routes/v1/Prescription/prescription.js
+++ routes/v1/Prescription/prescription.js
@@ -5,8 +5,10 @@ const verify = require('../../../verifyToken')
 
 
 // upload a new prescription
+// this is done by a user on the ecentials themselves.
 router.post('/new-prescription', verify, async (req, res) => {
-   const { user_id, status, image } = req.body;
+   const user_id = req.user._id;
+   const { status, image } = req.body;
    
    if (!!user_id && !!status ) {
     await Prescription.create({
@@ -20,6 +22,27 @@ router.post('/new-prescription', verify, async (req, res) => {
    } else {
        return res.status(400).json({message: "Please provide the necessary data"});
    }
+});
+
+
+// upload a new prescription - pharmacists
+// this upload is done by the pharmacists on behalf of the user.
+// if the user details is not in the system, the pharmacists add them.
+router.post('/upload-prescription-by-pharmacists', verify, async (req, res) => {
+    const { user_id, status, store_id } = req.body; 
+
+    if (!!user_id && !!store_id ) {
+        await Prescription.create({
+            user_id, status, store_id
+        }, (err, result) => {
+            if (err) {
+                return res.status(400).json({message: 'Failed to save prescription'})
+            }
+            return res.status(200).json({message: 'success', data: result});
+        } );
+       } else {
+           return res.status(400).json({message: "Please provide the necessary data"});
+       }
 });
 
 
