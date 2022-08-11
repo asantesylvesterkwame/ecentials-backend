91fd2ae8c8910b214480d1b882c40cba892851ec
lateofrederick <lateofrederick@gmail.com>
2022-08-11 20:13:35 +0000

    retrieve total income made by pharmacy

--- routes/v1/Pharmacy/orders.js
+++ routes/v1/Pharmacy/orders.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const Orders = require('../../../private/schemas/Orders');
+const PaymentTransaction = require('../../../private/schemas/PaymentTransaction');
 const verify = require('../../../verifyToken')
 
 
@@ -24,6 +25,28 @@ router.get('/total-products-sold', verify, async (req, res) => {
     await Orders.find({ store_id, fulfilled: true }).count((err, result) => {
         if (err) {
             return res.status(400).json({ message: "Could not retrieve total products sold." });
+        }
+        return res.status(200).json({ message: "success", data: result });
+    }).clone();
+});
+
+
+// total income made by a pharmacy
+// this implementation might change in the future as this is 
+// calculated only based on orders made in the system.
+router.get('/total-income-made-by-pharmacy', verify, async (req, res) => {
+    const { facility_id } = req.body;
+
+    await PaymentTransaction.aggregate([{
+        "$group": {
+            facility_id,
+            "total": {
+                "$sum": "$amount_paid"
+            }
+        }
+    }], (err, result) => {
+        if (err) {
+            return res.status(400).json({ message: "Failed to get total income" });
         }
         return res.status(200).json({ message: "success", data: result });
     }).clone();
