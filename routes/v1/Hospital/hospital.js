25043bb3789fb4088009e63a2fa14ac4f55e3218
lateofrederick <lateofrederick@gmail.com>
2022-08-16 19:19:43 +0000

    create a new hospital

---
+++ routes/v1/Hospital/hospital.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+
+const Hospital = require('../../../private/schemas/Hospital');
+const verify = require('../../../verifyToken')
+
+
+// create a new hospital using data from request body
+router.post('/add-new-hospital', (req, res) => {
+    const {
+        name,
+        address,
+        opening_hours,
+        phone_number,
+        gps_address,
+    } = req.body;
+
+    Hospital.create({
+        name, address, opening_hours, phone_number, gps_address
+    }, (err, result) => {
+        if (err) {
+            return res.status(200).json({ message: "Failed to create a hospital" });
+        }
+        return res.status(200).json({ message: "success", data: result });
+    })
+});
+
+
+module.exports = router;
