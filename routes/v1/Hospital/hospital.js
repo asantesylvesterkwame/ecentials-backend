e455dc3831eccf9e86d855f224f27251e6fad4b9
lateofrederick <lateofrederick@gmail.com>
2022-08-28 14:58:20 +0000

    allow user to search for hospital

--- routes/v1/Hospital/hospital.js
+++ routes/v1/Hospital/hospital.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const multer = require('multer')
 
 const Hospital = require('../../../private/schemas/Hospital');
-
+const verify = require('../../../verifyToken');
 
 const upload = multer({ dest: './uploads' });
 
@@ -42,7 +42,7 @@ router.post('/upload-hospital-images', upload.array("pictures", 15), async (req,
             const newImg = fs.readFileSync(picture.path);
             const encImg = newImg.toString('base64');
             const image = {
-                data: Buffer.from(encImg, 'base64'),
+                image: Buffer.from(encImg, 'base64'),
                 contentType: picture.mimetype
             }
             images.push(image);
@@ -55,5 +55,31 @@ router.post('/upload-hospital-images', upload.array("pictures", 15), async (req,
         return res.status(200).json({ message: "success", data: result });
     }).clone();
 })
+
+
+// search for a hospital using specific keywords like name, address, 
+// gps_address, phone number
+router.post('/search-for-hospital', verify, async (req, res) => {
+    const { search_text } = req.body;
+
+    try {
+        const results =  await Hospital.find({ 
+            "$or": [
+                {name: { $regex: search_text, '$options' : 'i' }},
+                {address: { $regex: search_text, '$options' : 'i' }},
+                {gps_address: { $regex: search_text, '$options' : 'i' }},
+                {phone_number: { $regex: search_text }}
+            ]
+        });
+    
+        if (results == null) {
+            return res.status(400).json({ message: "No hospital found"})
+        }
+        return res.status(400).json({ message: "success", data: results });
+    } catch (error) {
+        return res.status(400).json({ message: "Something went wrong. Please try again" });
+    }
+});
+
 
 module.exports = router;
