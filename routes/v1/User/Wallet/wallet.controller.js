950b7df373bc200e9ba92ea0a04a0902fe942e71
lateofrederick <lateofrederick@gmail.com>
2022-09-01 14:47:35 +0000

    get wallet balance

--- routes/v1/User/Wallet/wallet.controller.js
+++ routes/v1/User/Wallet/wallet.controller.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const { createWallet } = require('../../../../private/services/wallet/wallet.service');
+const { createWallet, getWalletBalance } = require('../../../../private/services/wallet/wallet.service');
 const verify = require('../../../../verifyToken')
 
 
@@ -9,6 +9,16 @@ router.post('/create-ecentials-wallet', verify, async (req, res, next) => {
     const user_id = req.user._id;
     try {
         res.json(await createWallet(user_id))
+    } catch (error) {
+        next(error);
+    }
+});
+
+
+router.get('/get-ecentials-wallet-balance', verify, async (req, res, next) => {
+    const user_id = req.user._id;
+    try {
+        res.json(await getWalletBalance(user_id));
     } catch (error) {
         next(error);
     }
