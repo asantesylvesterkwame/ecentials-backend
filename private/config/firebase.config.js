9f5bf262a56328f7d945ef96cd717bc37785b52d
lateofrederick <lateofrederick@gmail.com>
2022-09-14 15:55:09 +0000

    add firebase configuration

---
+++ private/config/firebase.config.js
@@ -0,0 +1,18 @@
+const { initializeApp } = require("firebase/app");
+
+require('dotenv').config();
+
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSENGER_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+module.exports = app;
