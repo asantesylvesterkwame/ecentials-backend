dd42c0fa1e8908109907744347086a841de84711
lateofrederick <lateofrederick@gmail.com>
2022-10-15 13:45:51 +0000

    set local and production mongo database

--- private/database/mongodb.js
+++ private/database/mongodb.js
@@ -5,7 +5,7 @@ module.exports = (db_name) => {
     //     console.log("MongoDB Connection Successful")
     // }, e => console.error(e))
 
-    return mongoose.connect(`mongodb+srv://ecentials:0Ef1RfIbYghJoKJK@cluster0.k0eejno.mongodb.net/?retryWrites=true&w=majority`, () => {
+    return mongoose.connect(`${process.env.DB_URI}`, () => {
         console.log("MongoDB Connection Successful")
     }, e => console.error(e))
 
