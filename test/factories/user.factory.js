debc4e5bbe9692cf2326bbe004e6e07e20e0eca8
lateofrederick <lateofrederick@gmail.com>
2022-10-18 20:38:31 +0000

    create factories for testing

---
+++ test/factories/user.factory.js
@@ -0,0 +1,22 @@
+const User = require("../../private/schemas/User");
+
+class UserFactory {
+    constructor(name, occupation) {
+        this.name = name,
+        this.occupation = occupation
+    }
+
+    static async create(userfactory) {
+        const user = await User.create({
+            personal: {
+                name: userfactory.name,
+                occupation: userfactory.occupation,
+            },
+            email: "test_email",
+            password: "56738euijasdfskajfs"
+        });
+        return user
+    }
+}
+
+module.exports = UserFactory;
