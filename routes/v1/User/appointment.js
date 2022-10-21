3ad13b6b23739f8d0f5979c914507ab1e4bc9743
lateofrederick <lateofrederick@gmail.com>
2022-10-21 22:51:39 +0000

    refactor appointments endpoint

--- routes/v1/User/appointment.js
+++ routes/v1/User/appointment.js
@@ -2,19 +2,25 @@ const router = require('express').Router();
 
 const { verify } = require('../../../verifyToken');
 const Appointments = require('../../../private/schemas/Appointments');
+const { getUserAppointments } = require('../../../private/services/User/Appointments/appointments.service');
 
 
-router.post('', verify, async (req, res) => {
+router.post('', verify, async (req, res, next) => {
     const user_id = req.user._id;
 
     const { status } = req.body;
 
-    Appointments.find({ user_id, status }, (err, result) => {
-        if (err) {
-            return res.status(400).json({ message: 'Failed to load appointments' });
-        }
-        return res.status(200).send({ message: 'success', data: result });
-    }).clone();
+    // Appointments.find({ user_id, status }, (err, result) => {
+    //     if (err) {
+    //         return res.status(400).json({ message: 'Failed to load appointments' });
+    //     }
+    //     return res.status(200).send({ message: 'success', data: result });
+    // }).clone();
+    try {
+        return res.status(200).json(await getUserAppointments({ user_id, status }));
+    } catch (error) {
+        next(error);
+    }
 });
 
 
@@ -23,6 +29,7 @@ router.post('/book-an-appointment', verify, async (req, res) => {
     const user_id = req.user._id;
     const {
         staff_id,
+        facility_id,
         date,
         time,
         status,
@@ -30,7 +37,7 @@ router.post('/book-an-appointment', verify, async (req, res) => {
     } = req.body;
 
     await Appointments.create({
-        user_id, staff_id, date, time, status, appoint_type
+        user_id, staff_id, date, time, status, appoint_type, facility_id
     }, (err, result) => {
         if (err) {
             return res.status(400).json({ message: "Failed to book appointment. Try again later", err });
