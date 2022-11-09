67e61432b504140baf03f4711d9781101fb34105
lateofrederick <lateofrederick@gmail.com>
2022-11-09 23:49:31 +0000

    remove redundant code

--- routes/v1/Pharmacy/pharmacy.js
+++ routes/v1/Pharmacy/pharmacy.js
@@ -25,34 +25,6 @@ router.get('', verify, async (req, res) => {
 
 // create a new pharmacy
 router.post('/create-new-pharmacy', verify, upload, async (req, res, next) => {
-    // const owner_id = req.user._id;
-    // const { 
-    //     store_pin, 
-    //     name, 
-    //     description, 
-    //     address, 
-    //     city, 
-    //     licence_no,
-    //     logo,
-    //     accept_nhis,
-    //     gps_lat,
-    //     gps_lng,
-    //  } = req.body;
-
-    //  if (!!name && !!licence_no && !!city) {
-    //      await Store.create({
-    //          owner_id, name, description, address, city, licence_no, logo, accept_nhis,
-    //          gps_lat, gps_lng, store_pin: encryptPassword(store_pin)
-    //      }, (err, result) => {
-    //          if (err) {
-    //              return res.status(400).json({ message: 'Failed to create pharmacy.' });
-    //          }
-    //          return res.status(200).json({ message: 'success', data: result});
-    //      });
-    //  } else {
-    //      return res.status(403).json({ message: 'Please make sure the correct data is provided' });
-    //  }
-
      try {
         return res.status(200).json(await createNewPharmacy({ req }))
      } catch (error) {
