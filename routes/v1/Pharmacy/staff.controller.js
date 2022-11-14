62165485a4dad2cf318de1693e822accbdd88cf2
lateofrederick <lateofrederick@gmail.com>
2022-11-14 21:47:38 +0000

    create new staff

--- routes/v1/Pharmacy/staff.controller.js
+++ routes/v1/Pharmacy/staff.controller.js
@@ -1,8 +1,15 @@
 const router = require('express').Router();
+const multer = require('multer')
 
-const { getPharmacyStaff } = require('../../../private/services/Pharmacy/Staff/staff.service');
-const verify = require('../../../verifyToken');
+const { getPharmacyStaff, createPharmacyStaff } = require('../../../private/services/Pharmacy/Staff/staff.service');
+const { verify } = require("../../../verifyToken");
 
+const storage = multer.memoryStorage()
+const upload = multer({ storage }).fields([
+    { name: 'photo', maxCount: 1},
+    { name: 'cv', maxCount: 1},
+    { name: 'certificate', maxCount: 1}
+])
 
 // fetch all staff registered to a pharmacy
 router.post('/fetch-pharmacy-staff', verify, async (req, res, next) => {
@@ -11,6 +18,15 @@ router.post('/fetch-pharmacy-staff', verify, async (req, res, next) => {
         return res.status(200).json(await getPharmacyStaff({ facility_id }));
     } catch (error) {
         next(error);
+    }
+})
+
+// create new pharmacy staff
+router.post('/add-new-staff', verify, upload, async (req, res, next) => {
+    try {
+        return res.status(200).json(await createPharmacyStaff({ req }))
+    } catch (error) {
+       next(error) 
     }
 })
 
