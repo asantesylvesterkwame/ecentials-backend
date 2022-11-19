e867096ce99bb0d11b844288f5b810f52533110b
lateofrederick <lateofrederick@gmail.com>
2022-11-19 22:11:02 +0000

    add delete, update controllers

--- routes/v1/User/UserAddress/user_address.controller.js
+++ routes/v1/User/UserAddress/user_address.controller.js
@@ -3,6 +3,8 @@ const router = require("express").Router();
 const {
   addNewShippingAddress,
   getUserShippingAddresses,
+  updateShippingAddress,
+  deleteShippingAddress,
 } = require("../../../../private/services/UserAddress/user_address.service");
 const { verify } = require("../../../../verifyToken");
 
@@ -38,5 +40,21 @@ router.get("/fetch-all-shipping-addresses", verify, async (req, res, next) => {
     next(error);
   }
 });
+
+router.post('/update-shipping-address', verify, async (req, res, next) => {
+  try {
+    return res.status(200).json(await updateShippingAddress({ req }))
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post('/delete-shipping-address', verify, async (req, res, next) => {
+  try {
+    return res.json(await deleteShippingAddress({ req }))
+  } catch (error) {
+    next(error)
+  }
+})
 
 module.exports = router;
