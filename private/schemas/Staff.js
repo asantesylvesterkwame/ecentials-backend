b48dc4ba956c755da3e8f4172f21c9bc934a6bc9
unknown <aopoku255gmail.com>
2022-11-25 18:13:45 +0000

    removed required username in Schema

--- private/schemas/Staff.js
+++ private/schemas/Staff.js
@@ -1,140 +1,139 @@
 const mongoose = require("mongoose");
 
-const staffSchema = new mongoose.Schema({
+const staffSchema = new mongoose.Schema(
+  {
     facility_id: {
-        type: mongoose.SchemaTypes.ObjectId,
-        required: true
+      type: mongoose.SchemaTypes.ObjectId,
+      required: true,
     },
     facility_type: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     // hospital_id: {
     //     type: mongoose.SchemaTypes.ObjectId,
     //     required: true
     // },
     employee_id: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     first_name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     last_name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        text: true
+      type: String,
+      required: true,
+      text: true,
     },
     phone_number: {
-        type: String,
-        required: false
+      type: String,
+      required: false,
     },
     address: {
-        type: String,
-        required: true,
-        text: true
+      type: String,
+      required: true,
+      text: true,
     },
     photo: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     date_of_birth: {
-        type: Date,
-        required: true
+      type: Date,
+      required: true,
     },
     place_of_birth: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     ghana_card_number: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     pay_grade: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     mode_of_payment: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     department: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     supervisor: {
-        type: mongoose.SchemaTypes.ObjectId,
-        required: false
+      type: mongoose.SchemaTypes.ObjectId,
+      required: false,
     },
     university: {
-        type: String,
-        required: false
+      type: String,
+      required: false,
     },
     degree: {
-        type: String,
-        required: false
+      type: String,
+      required: false,
     },
     start_date: {
-        type: Date,
-        required: true
+      type: Date,
+      required: true,
     },
     end_date: {
-        type: Date,
-        required: false
+      type: Date,
+      required: false,
     },
     city: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     cv: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     certificate: {
-        type: String,
-        required: false
+      type: String,
+      required: false,
     },
     specification: {
-        type: String,
-        required: false
+      type: String,
+      required: false,
     },
     experience: {
-        type: String,
-        required: false
+      type: String,
+      required: false,
     },
     about: {
-        type: String,
-        required: false,
-        text: true
+      type: String,
+      required: false,
+      text: true,
     },
     staff_type: {
-        type: String,
-        required: true,
-        text: true
+      type: String,
+      required: true,
+      text: true,
     },
     availability: {
-        type: String,
-        required: false
-    },
-    username: {
-        type: String,
-        required: true
+      type: String,
+      required: false,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     role: {
-        type: String,
-        required: false,
-        default: "Staff"
+      type: String,
+      required: false,
+      default: "Staff",
     },
-    privileges: []
-}, { timestamps: true });
+    privileges: [],
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Staff", staffSchema);
