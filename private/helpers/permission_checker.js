0f84545e0843ff2a1e17f89c6850df581182a3de
lateofrederick <lateofrederick@gmail.com>
2022-12-06 20:30:18 +0000

    refactor checker

--- private/helpers/permission_checker.js
+++ private/helpers/permission_checker.js
@@ -1,7 +1,19 @@
-function permissionChecker(...privileges) {
-    return (req, res, next) => {
-        const { user } = req
+const Staff = require("../schemas/Staff")
 
+function isStaffChecker(...privileges) {
+    return async (req, res, next) => {
+        const { user } = req
+        
+        const staff_instance = await Staff.findById(user._id)
+        if (user && privileges.includes(staff_instance.privileges)) {
+            next()
+        } else {
+            res.status(403).json({ message: "Forbidden" })
+        }
         
     }
+}
+
+module.exports = {
+    isStaffChecker,
 }
