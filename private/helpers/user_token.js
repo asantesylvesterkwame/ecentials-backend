91ced4a25af18b40d5fc09729d26aa7e9b5593b0
lateofrederick <lateofrederick@gmail.com>
2022-12-10 23:34:51 +0000

    add token helpers

---
+++ private/helpers/user_token.js
@@ -0,0 +1,56 @@
+const jwt = require('jsonwebtoken')
+
+const UserToken = require('../schemas/UserToken')
+
+
+const verifyRefreshToken = (refreshToken) => {
+    const privateKey = process.env.SECRET
+
+    return new Promise((resolve, reject) => {
+        UserToken.findOne({ token: refreshToken }, (err, doc))
+
+        if (!doc)
+            return reject({ error: true, message: "Invalid refresh token" });
+
+        jwt.verify(refreshToken, privateKey, (err, tokenDetails) => {
+            if (err)
+                return reject({ error: true, message: "Invalid refresh token" })
+            
+            resolve({
+                tokenDetails,
+                error: false,
+                message: "Valid refresh token",
+            });
+        })
+    })
+}
+
+async function generateTokens(user) {
+    try {
+        const payload = { _id: user._id, roles: user.roles };
+        const accessToken = jwt.sign(
+            payload,
+            process.env.SECRET,
+            { expiresIn: "14m" }
+        )
+
+        const refreshToken = jwt.sign(
+            payload,
+            process.env.SECRET,
+            { expiresIn: "30d" }
+        )
+
+        const userToken = await UserToken.findOne({ user_id: user._id });
+        if (userToken) await userToken.remove()
+        
+        await new UserToken({ user_id: user._id, token: refreshToken }).save()
+        return Promise.resolve({ accessToken, refreshToken })
+    } catch (error) {
+        return Promise.reject(err);
+    }
+}
+
+module.exports = {
+    verifyRefreshToken,
+    generateTokens
+}
