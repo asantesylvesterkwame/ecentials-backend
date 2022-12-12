82b557451f4eab827fe45bd344af1bd1cdac76d0
lateofrederick <lateofrederick@gmail.com>
2022-12-12 21:55:11 +0000

    make constants returned for  drug data

---
+++ private/helpers/constants.js
@@ -0,0 +1,25 @@
+const DRUG_RETURN_DATA = {
+    "name": 1,
+    "price": 1,
+    "selling_price": 1,
+    "description": 1,
+    "medicine_group": 1,
+    "dosage": 1,
+    "total_stock": 1,
+    "views": 1,
+    "nhis": 1,
+    "expiry_date": 1,
+    "discount": 1,
+    "image": 1,
+    "category_name": "$category.name",
+    "category_status": "$category.status",
+    "store_name": "$store.name",
+    "store_location": "$store.location",
+    "store_phone_number": "$store.phone_number",
+    "store_open_hours": "$store.open_hours",
+    "store_logo": "$store.logo"
+}
+
+module.exports = {
+    DRUG_RETURN_DATA
+}
