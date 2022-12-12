e09debef2854481eb40b9a6b19d175c5dccef049
lateofrederick <lateofrederick@gmail.com>
2022-12-12 21:10:35 +0000

    get reviews about an entity

--- private/services/Reviews/reviews.service.js
+++ private/services/Reviews/reviews.service.js
@@ -1,3 +1,5 @@
+const ObjectId = require("mongoose").Types.ObjectId
+
 const Ratings = require("../../schemas/Ratings");
 
 async function addNewReview({ req }) {
@@ -13,6 +15,53 @@ async function addNewReview({ req }) {
     }
 }
 
+async function getReviews({ req }) {
+    try {
+        const result = await Ratings.aggregate([
+            { 
+                $match: { 
+                    recipient_type: req.body.recipient_type,
+                    recipient_id: ObjectId(req.body.recipient_id)
+                }
+            },
+            {
+                $lookup: {
+                    from: 'users',
+                    localField: 'reviewer_id',
+                    foreignField: '_id',
+                    as: 'User'
+                }
+            },
+            {
+                $unwind: {
+                    path: '$User',
+                    preserveNullAndEmptyArrays: true
+                }
+            },
+            {
+                $project: {
+                    '_id': 1,
+                    'recipient_id': 1,
+                    'recipient_type': 1,
+                    'rating': 1,
+                    'message': 1,
+                    'reviewer_id': 1,
+                    'reviewer_name': '$User.personal.name',
+                    'createdAt': 1
+                }
+            }
+        ])
+
+        return { 
+            status: 'success', 
+            message: 'reviews successfully retrieved', 
+            data: result 
+        }
+    } catch (error) {
+        return { status: 'error', message: 'an error occurred, please try again' }
+    }
+}
 module.exports = {
-    addNewReview
+    addNewReview,
+    getReviews
 }
