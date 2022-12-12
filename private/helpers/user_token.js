496ba14cbe2a5d151395ba505e08fbb7ecfa21c9
lateofrederick <lateofrederick@gmail.com>
2022-12-12 07:56:23 +0000

    refactor code

--- private/helpers/user_token.js
+++ private/helpers/user_token.js
@@ -7,20 +7,20 @@ const verifyRefreshToken = (refreshToken) => {
     const privateKey = process.env.SECRET
 
     return new Promise((resolve, reject) => {
-        UserToken.findOne({ token: refreshToken }, (err, doc))
-
-        if (!doc)
+        UserToken.findOne({ token: refreshToken }, (err, doc) => {
+            if (!doc)
             return reject({ error: true, message: "Invalid refresh token" });
 
-        jwt.verify(refreshToken, privateKey, (err, tokenDetails) => {
-            if (err)
-                return reject({ error: true, message: "Invalid refresh token" })
-            
-            resolve({
-                tokenDetails,
-                error: false,
-                message: "Valid refresh token",
-            });
+            jwt.verify(refreshToken, privateKey, (err, tokenDetails) => {
+                if (err)
+                    return reject({ error: true, message: "Invalid refresh token" })
+                
+                resolve({
+                    tokenDetails,
+                    error: false,
+                    message: "Valid refresh token",
+                });
+            })
         })
     })
 }
