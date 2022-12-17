9d6014b507ab62966f8ad26b1b9c89e2ce12c872
lateofrederick <lateofrederick@gmail.com>
2022-12-17 19:02:25 +0000

    remove log

--- private/services/User/Bookmark/bookmark.service.js
+++ private/services/User/Bookmark/bookmark.service.js
@@ -3,7 +3,7 @@ const Bookmark = require("../../../schemas/Bookmark")
 async function getBookmarkDetails(req) {
     try {
         const result = await Bookmark.findById(req.body.bookmark_id)
-        console.log(req.body.bookmark_id)
+        
         return { status: 'success', message: 'bookmark data retrieved', data: result}
     } catch (error) {
         return { status: 'error', message: 'an error occurred, please try again' }
