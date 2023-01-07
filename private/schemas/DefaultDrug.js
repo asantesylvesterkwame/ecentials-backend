886f8abefb078bf203484b79f774b2273248ad8b
lateofrederick <lateofrederick@gmail.com>
2023-01-07 15:34:34 +0000

    create model to represent default drugs

---
+++ private/schemas/DefaultDrug.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+const defaulDrugSchema = new mongoose.Schema({
+    drug_code: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    dosage_form: {
+        type: String,
+        required: true
+    },
+    dosage: {
+        type: String,
+        required: true
+    },
+    unit_of_pricing: {
+        type: String,
+        required: false
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: String,
+        required: false
+    },
+    level_of_prescribing: {
+        type: String,
+        required: true
+    },
+}, { timestamps: true })
+
+module.exports = mongoose.model('DefaultDrug', defaulDrugSchema)
