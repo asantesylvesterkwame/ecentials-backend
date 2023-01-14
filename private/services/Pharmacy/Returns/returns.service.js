7571b23105750f73adc1d00596b30b650f950c38
unknown <aopoku255gmail.com>
2023-01-14 19:29:02 +0000

    Addded Returns

--- private/services/Pharmacy/Returns/returns.service.js
+++ private/services/Pharmacy/Returns/returns.service.js
@@ -1,29 +1,49 @@
+const { Data } = require("@react-google-maps/api");
+const Drug = require("../../../schemas/Drug");
 const Orders = require("../../../schemas/Orders");
 const Returns = require("../../../schemas/Returns");
 
-async function fetchReturns({ store_id, invoice_number }) {
+async function addReturns({ store_id, invoice_number }) {
+  const invoice_num = invoice_number;
   try {
-    const results = Orders.find({ store_id, invoice_number });
-    const {
-      store_id,
-      order_code,
+    const result = await Orders.find({ store_id });
+    const results = result.filter(
+      ({ invoice_number }) => invoice_number == invoice_num
+    );
+    const newresults = await Returns.create({
       invoice_number,
-      payment_type,
-      grand_total,
-      delivery_method,
-    } = results;
-    const result = Returns.create({
-      store_id,
-      order_code,
-      invoice_number,
-      payment_type,
-      grand_total,
-      delivery_method,
     });
-    return { message: result };
+
+    results[0].products_summary.forEach(async (item) => {
+      // reduce total drug stock
+      await Drug.updateOne(
+        {
+          _id: item.drug_id,
+        },
+        {
+          $inc: { total_stock: +item.quantity },
+        }
+      );
+    });
+
+    return { message: "success" };
   } catch (error) {
     return { message: error };
   }
 }
 
-module.exports = { fetchReturns };
+async function fetchReturns({ store_id }) {
+  let newResults = [];
+  try {
+    const results = await Returns.find({ store_id });
+    for (let result of results) {
+      newResults.push(await Orders.find({ ...result.invoice_number }));
+      // console.log(result.invoice_number);
+    }
+    return { message: "success", data: newResults[0] };
+  } catch (error) {
+    return { message: error };
+  }
+}
+
+module.exports = { addReturns, fetchReturns };
--- routes/v1/Pharmacy/Returns/returns.controller.js
+++ routes/v1/Pharmacy/Returns/returns.controller.js
@@ -1,14 +1,23 @@
 const router = require("express").Router();
 const {
+  addReturns,
   fetchReturns,
 } = require("../../../../private/services/Pharmacy/Returns/returns.service");
 
 router.post("", async (req, res, next) => {
   const { store_id, invoice_number } = req.body;
   try {
-    return res
-      .status(200)
-      .json(await fetchReturns({ store_id, invoice_number }));
+    return res.status(200).json(await addReturns({ store_id, invoice_number }));
+  } catch (error) {
+    next(error);
+  }
+});
+
+// fetch Returns
+router.post("/fetch-returns", async (req, res, next) => {
+  const { store_id } = req.body;
+  try {
+    return res.status(200).json(await fetchReturns({ store_id }));
   } catch (error) {
     next(error);
   }
