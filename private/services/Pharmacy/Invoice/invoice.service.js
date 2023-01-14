a9b2efbdbf9871343eede25d94e422df756ff51c
unknown <aopoku255gmail.com>
2023-01-14 16:11:01 +0000

    Invoice filter

--- private/services/Pharmacy/Invoice/invoice.service.js
+++ private/services/Pharmacy/Invoice/invoice.service.js
@@ -4,7 +4,7 @@ async function fetchInvoice({ store_id }) {
   try {
     const result = await Orders.find({ store_id });
     const results = result.filter(
-      ({ delivery_method }) => delivery_method === "Invoice"
+      ({ delivery_method }) => delivery_method === "Pickup"
     );
     return { message: "success", data: results };
   } catch (error) {
