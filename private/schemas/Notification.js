83fdc95a2266d5984313563672a71e37901c632f
lateofrederick <lateofrederick@gmail.com>
2023-01-30 15:34:33 +0000

    set image field as not required

--- private/schemas/Notification.js
+++ private/schemas/Notification.js
@@ -12,7 +12,7 @@ const notificationSchema = new mongoose.Schema({
     },
     image: {
         type: String, 
-        required: true
+        required: false
     }, 
     title: {
         type: String, 
