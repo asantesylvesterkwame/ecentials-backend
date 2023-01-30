481906d2820512b8f4d59cbc45e2154abe9c2c8d
lateofrederick <lateofrederick@gmail.com>
2023-01-30 15:37:10 +0000

    send push notification to user when order is cancelled

--- private/services/Pharmacy/Orders/orders.service.js
+++ private/services/Pharmacy/Orders/orders.service.js
@@ -1,5 +1,8 @@
 const router = require("express").Router();
 const Orders = require("../../../../private/schemas/Orders");
+const {
+  sendFCMessage,
+} = require("../../User/Notification/notification.service");
 
 async function cancelOrder({ req }) {
   const { order_code } = req.body;
@@ -9,13 +12,48 @@ async function cancelOrder({ req }) {
       { order_code },
       { $set: { order_status: "Cancelled" } }
     );
-
-    if (result != null) {
+    const user = await _getUser(order_code);
+    
+    await sendFCMessage(user.user_token, "Order cancelled", req.body.message);
+    
+    if (result.modifiedCount > 0) {
       return { message: "success", data: result };
     }
-    return { message: "Failed to create checkout item.", data: err };
+    
+    return { message: "Failed to cancel checkout item.", data: err };
   } catch (error) {
     return { message: "Failed to create checkout item.", data: error };
+  }
+}
+
+async function _getUser(order_code) {
+  try {
+    const result = await Orders.aggregate([
+      { $match: { order_code } },
+      {
+        $lookup: {
+          from: "users",
+          localField: "user_id",
+          foreignField: "_id",
+          as: "user",
+        },
+      },
+      {
+        $unwind: {
+          path: "$user",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $project: {
+          user_token: "$user.fcm_token",
+          user_id: "$user._id",
+        },
+      },
+    ]);
+    return result[0];
+  } catch (error) {
+    throw new Error(error);
   }
 }
 
