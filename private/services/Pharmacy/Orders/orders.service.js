10663827d5ed72cf34189c02028a88eb01757bce
lateofrederick <lateofrederick@gmail.com>
2023-01-30 16:00:52 +0000

    create a notification instance

--- private/services/Pharmacy/Orders/orders.service.js
+++ private/services/Pharmacy/Orders/orders.service.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Orders = require("../../../../private/schemas/Orders");
+const Notification = require("../../../schemas/Notification");
 const {
   sendFCMessage,
 } = require("../../User/Notification/notification.service");
@@ -13,13 +14,24 @@ async function cancelOrder({ req }) {
       { $set: { order_status: "Cancelled" } }
     );
     const user = await _getUser(order_code);
+
+    const sendNotification = sendFCMessage(
+      user.user_token,
+      "Order cancelled",
+      req.body.message
+    );
+    const createNotification = _createNewNotification(
+      user.user_id,
+      "Order cancelled",
+      req.body.message
+    );
     
-    await sendFCMessage(user.user_token, "Order cancelled", req.body.message);
-    
+    Promise.all([sendNotification, createNotification]).catch((e) => { throw new Error(e) })
+
     if (result.modifiedCount > 0) {
       return { message: "success", data: result };
     }
-    
+
     return { message: "Failed to cancel checkout item.", data: err };
   } catch (error) {
     return { message: "Failed to create checkout item.", data: error };
@@ -52,6 +64,18 @@ async function _getUser(order_code) {
       },
     ]);
     return result[0];
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+async function _createNewNotification(user_id, title, message) {
+  try {
+    await Notification.create({
+      user_id,
+      title,
+      message,
+    });
   } catch (error) {
     throw new Error(error);
   }
