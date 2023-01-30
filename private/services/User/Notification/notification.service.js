a6b9876f792e447cfbde348f704514cd81fffc51
lateofrederick <lateofrederick@gmail.com>
2023-01-30 15:35:34 +0000

    send push notification to user

--- private/services/User/Notification/notification.service.js
+++ private/services/User/Notification/notification.service.js
@@ -1,21 +1,37 @@
+const { admin } = require("../../../config/firebase_admin.config");
 const User = require("../../../schemas/User");
 
 async function setFCMToken(req) {
-    try {
-        await User.findByIdAndUpdate(
-            req.user._id,
-            {
-                $set: {
-                    fcm_token: req.body.fcm_token
-                }
-            }
-        )
-        return { status: 'success', message: 'fcm token set successfully' }
-    } catch (error) {
-        return { status: 'error', message: 'an error occurred, please try again' }
-    }
+  try {
+    await User.findByIdAndUpdate(req.user._id, {
+      $set: {
+        fcm_token: req.body.fcm_token,
+      },
+    });
+    return { status: "success", message: "fcm token set successfully" };
+  } catch (error) {
+    return { status: "error", message: "an error occurred, please try again" };
+  }
+}
+
+async function sendFCMessage(fcmToken, title, message) {
+  try {
+    var payload = {
+      notification: {
+        title: title,
+        body: message,
+      },
+    };
+    admin.messaging().sendToDevice(fcmToken, payload, {
+      priority: "high",
+      timeToLive: 60 * 60 * 24,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
 }
 
 module.exports = {
-    setFCMToken
-}
+  setFCMToken,
+  sendFCMessage,
+};
