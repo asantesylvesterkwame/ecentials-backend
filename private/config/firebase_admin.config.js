7a588f744623bb086e019e74bdefb2c207d2674f
lateofrederick <lateofrederick@gmail.com>
2023-02-01 13:32:12 +0000

    remove hard coded credentials

--- private/config/firebase_admin.config.js
+++ private/config/firebase_admin.config.js
@@ -1,8 +1,28 @@
+require('dotenv').config();
 const admin = require("firebase-admin");
 const serviceAccount = require("./service.json");
 
+console.log(process.env.SERVICE_PRIVATE_KEY);
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(
+    // {
+    //   "type": process.env.SECRET_TYPE,
+    //   "project_id": process.env.SERVICE_PROJECT_ID,
+    //   "private_key_id": process.env.SERVICE_PRIVATE_KEY_ID,
+    //   "private_key": process.env.SECRET_PRIVATE_KEY,
+    //   "client_email": process.env.SERVICE_CLIENT_EMAIL,
+    //   "client_id": process.env.SERVICE_CLIENT_ID,
+    //   "auth_uri": process.env.SERVICE_AUTH_URI,
+    //   "token_uri": process.env.SERVICE_TOKEN_URI,
+    //   "auth_provider_x509_cert_url": process.env.SERVICE_AUTH_PROVIDER_X509_CERT_URL,
+    //   "client_x509_cert_url": process.env.SERVICE_CLIENT_X509_CERT_URL
+    // }
+    {
+      privateKey: process.env.SERVICE_PRIVATE_KEY,
+      projectId: process.env.SERVICE_PROJECT_ID,
+      clientEmail: process.env.SERVICE_CLIENT_EMAIL,
+    }
+  ),
 });
 
 module.exports.admin = admin;
