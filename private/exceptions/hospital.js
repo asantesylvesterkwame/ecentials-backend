f57acb7ec6a645e7d415e2853472541dc668b5aa
lateofrederick <lateofrederick@gmail.com>
2023-05-31 16:24:44 +0000

    lateo: export class

--- private/exceptions/hospital.js
+++ private/exceptions/hospital.js
@@ -1,6 +1,10 @@
-class HospitalAppointException extends Error {
+class HospitalAppointmentException extends Error {
     constructor(message) {
         super(message);
         this.name = "HospitalAppointException";
     }
+}
+
+module.exports = {
+    HospitalAppointmentException,
 }
