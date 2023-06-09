a1729d04d7a9a6c4b6caf71bdea3b058de728900
asantesylvesterkwame <intelligentson17@gmail.com>
2023-06-09 10:37:06 +0000

    Created a get request to get all invoice data

--- private/services/Pharmacy/Invoice/invoice.service.js
+++ private/services/Pharmacy/Invoice/invoice.service.js
@@ -1,21 +1,35 @@
 /* eslint-disable */
 
-const Drug = require("../../../schemas/Drug");
-const Invoice = require("../../../schemas/Invoice");
-const Orders = require("../../../schemas/Orders");
+const Drug = require('../../../schemas/Drug');
+const Invoice = require('../../../schemas/Invoice');
+const Orders = require('../../../schemas/Orders');
 const {
   generateOrderCode,
   generateInvoiceNumber,
   // fetchLastInvoiceNumber,
-} = require("../../User/Orders/orders.service");
+} = require('../../User/Orders/orders.service');
 
 async function fetchInvoice({ store_id }) {
   try {
-    const result = await Invoice.find({ store_id }).sort({ createdAt: -1 });
+    const result = await Invoice.find({ store_id }).sort({
+      createdAt: -1,
+    });
 
-    return { message: "success", data: result };
+    return { message: 'success', data: result };
   } catch (error) {
-    return { message: "an error occurred, please try again" };
+    return { message: 'an error occurred, please try again' };
+  }
+}
+
+//Get Invoice Data
+async function fetchCustomerName(req, res) {
+  try {
+    const invoiceData = await Invoice.find();
+    if (invoiceData) {
+      return res.status(200).json({ invoiceData: invoiceData });
+    }
+  } catch (err) {
+    console.log(err.message);
   }
 }
 
@@ -23,7 +37,7 @@ async function addInvoice({ req }) {
   const { name } = req.body;
 
   try {
-    const order_code = await generateOrderCode("INVOICE", name);
+    const order_code = await generateOrderCode('INVOICE', name);
     const invoice_number = generateInvoiceNumber();
     await Invoice.create({
       ...req.body,
@@ -41,18 +55,18 @@ async function addInvoice({ req }) {
         )
     );
 
-    return { message: "success" };
+    return { message: 'success' };
   } catch (error) {
-    return { message: "Failed to add invoice.", data: error };
+    return { message: 'Failed to add invoice.', data: error };
   }
 }
 
 async function deleteInvoice(req) {
   try {
     await Invoice.findByIdAndDelete(req.body.invoice_id);
-    return { status: "success", message: "invoice deleted successfully" };
+    return { status: 'success', message: 'invoice deleted successfully' };
   } catch (error) {
-    return { status: "error", message: "an error occurred" };
+    return { status: 'error', message: 'an error occurred' };
   }
 }
 
@@ -61,25 +75,31 @@ async function searchInvoice(req) {
     const searchText = req.body.searchText;
     const filter = {
       $or: [
-        { customer_name: { $regex: searchText, $options: "i" } },
-        { invoice_number: { $regex: searchText, $options: "i" } },
-        { order_code: { $regex: searchText, $options: "i" } },
-        { payment_status: { $regex: searchText, $options: "i" } },
-        { order_status: { $regex: searchText, $options: "i" } },
+        { customer_name: { $regex: searchText, $options: 'i' } },
+        { invoice_number: { $regex: searchText, $options: 'i' } },
+        { order_code: { $regex: searchText, $options: 'i' } },
+        { payment_status: { $regex: searchText, $options: 'i' } },
+        { order_status: { $regex: searchText, $options: 'i' } },
       ],
     };
     const result = await Invoice.find(filter);
     return {
-      status: "success",
-      message: "data retrieved successfully",
+      status: 'success',
+      message: 'data retrieved successfully',
       data: result,
     };
   } catch (error) {
     return {
-      status: "error",
-      message: "an error occurred",
+      status: 'error',
+      message: 'an error occurred',
     };
   }
 }
 
-module.exports = { fetchInvoice, addInvoice, deleteInvoice, searchInvoice };
+module.exports = {
+  fetchInvoice,
+  addInvoice,
+  deleteInvoice,
+  searchInvoice,
+  fetchCustomerName,
+};
