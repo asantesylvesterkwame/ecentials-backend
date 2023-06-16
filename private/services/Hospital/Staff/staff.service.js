1e1d42ebb2bb37b80efdb02a67757c67641916f3
lateofrederick <lateofrederick@gmail.com>
2023-06-16 10:18:52 +0000

    lateo: fix linting issues

--- private/services/Hospital/Staff/staff.service.js
+++ private/services/Hospital/Staff/staff.service.js
@@ -15,64 +15,64 @@ const { uploadFile } = require("../../Firebase/imageUpload.service");
  * createHospitalStaff creates a new hospital staff
  */
 async function createHospitalStaff({ req }) {
-  //   try {
-  const employeeId = generateEmployeeID(
-    req.body.first_name,
-    req.body.last_name
-  );
+  try {
+    const employeeId = generateEmployeeID(
+      req.body.first_name,
+      req.body.last_name
+    );
 
-  const cvUrl = await uploadFile(
-    req.files.cv[0],
-    `hospitalStaff/${req.body.facility_id}/${employeeId}`
-  );
+    const cvUrl = await uploadFile(
+      req.files.cv[0],
+      `hospitalStaff/${req.body.facility_id}/${employeeId}`
+    );
 
-  const certificateUrl = await uploadFile(
-    req.files.certificate[0],
-    `hospitalStaff/${req.body.facility_id}/${employeeId}`
-  );
+    const certificateUrl = await uploadFile(
+      req.files.certificate[0],
+      `hospitalStaff/${req.body.facility_id}/${employeeId}`
+    );
 
-  const photoUrl = await uploadFile(
-    req.files.photo[0],
-    `hospitalStaff/${req.body.facility_id}/${employeeId}`
-  );
+    const photoUrl = await uploadFile(
+      req.files.photo[0],
+      `hospitalStaff/${req.body.facility_id}/${employeeId}`
+    );
 
-  const result = await Staff.create({
-    ...req.body,
-    cv: cvUrl,
-    certificate: certificateUrl,
-    photo: photoUrl,
-    password: encryptPassword(req.body.password),
-    employee_id: employeeId,
-  });
-
-  const facility = await getFacility(
-    req.body.facility_id,
-    req.body.facility_type
-  );
-
-  if (result != null) {
-    await sendAccountCreationEmail({
-      email: req.body.email,
-      password: req.body.password,
-      accountID: employeeId,
-      staffID: result._id,
-      staff_name: req.body.first_name,
-      facility_name: facility.name,
+    const result = await Staff.create({
+      ...req.body,
+      cv: cvUrl,
+      certificate: certificateUrl,
+      photo: photoUrl,
+      password: encryptPassword(req.body.password),
+      employee_id: employeeId,
     });
 
+    const facility = await getFacility(
+      req.body.facility_id,
+      req.body.facility_type
+    );
+
+    if (result != null) {
+      await sendAccountCreationEmail({
+        email: req.body.email,
+        password: req.body.password,
+        accountID: employeeId,
+        staffID: result._id,
+        staff_name: req.body.first_name,
+        facility_name: facility.name,
+      });
+
+      return {
+        status: "success",
+        message: "successfully added hospital staff",
+        data: result,
+      };
+    }
     return {
-      status: "success",
-      message: "successfully added hospital staff",
-      data: result,
+      status: "failed",
+      message: "failed to add hospital staff",
     };
+  } catch (error) {
+    throw new Error(`error adding new staff. ${error}`);
   }
-  return {
-    status: "failed",
-    message: "failed to add hospital staff",
-  };
-  //   } catch (error) {
-  //     throw new Error(`error adding new staff. ${error}`);
-  //   }
 }
 
 async function getHospitalStaff(req) {
